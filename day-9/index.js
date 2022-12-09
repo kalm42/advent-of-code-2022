37a01419816f75864df4f6f75e674483788bde11
Kyle Melton <1078490+kalm42@users.noreply.github.com>
2022-12-09 14:15:36 -0800

    part 2

--- day-9/index.js
+++ day-9/index.js
@@ -6,8 +6,13 @@ function main() {
 
     const travelDiary = new Set(['0,0']);
 
-    let tailLocation = {x: 0, y: 0};
-    const headLocation = {x: 0, y: 0};
+    let knots = new Array(10).fill('').map((knot) => ({x: 0, y: 0})).map((knot, i, rope) => {
+        // Assign each knot a reference to the next knot
+        if (i < rope.length - 1) {
+            knot.next = rope[i + 1];
+        }
+        return knot;
+    })
 
     data.split(/\r?\n/).forEach((line) => {
         const [direction, distance] = line.split(' ');
@@ -17,89 +22,94 @@ function main() {
             switch (direction) {
                 case 'U':
                     // move head up
-                    headLocation.y += 1;
+                    knots[0].y += 1;
                     break;
                 case 'D':
                     // move head down
-                    headLocation.y -= 1;
+                    knots[0].y -= 1;
                     break;
                 case 'L':
                     // move head left
-                    headLocation.x -= 1;
+                    knots[0].x -= 1;
                     break;
                 case 'R':
                     // move head right
-                    headLocation.x += 1;
+                    knots[0].x += 1;
                     break;
             }
-            tailLocation = moveTail(headLocation, tailLocation);
-            travelDiary.add(`${tailLocation.x},${tailLocation.y}`);
+            knots = moveTail(knots);
+            travelDiary.add(`${knots.at(-1).x},${knots.at(-1).y}`);
         }
     });
 
     const end = Date.now();
-    console.log({travelDiary: travelDiary.size, time: end - start})
+    console.log({travelDiary: travelDiary.size, knots, time: end - start})
 }
 
 main();
 
 // return the new tail location
-function moveTail(headLocation, tailLocation) {
-    let newTailLocation = {...tailLocation};
-    let xDiff = Math.abs(headLocation.x - tailLocation.x);
-    let yDiff = Math.abs(headLocation.y - tailLocation.y);
+function moveTail(knots) {
+    return knots.map((knot, index, rope) => {
+        // knot is the current knot, it's location was updated by the previous run
+        // We will use it's current location to update the knots behind it.
+        // early exit if we are at the end of the rope
+        if (index === rope.length - 1) return knot;
 
-    while (xDiff > 1 || yDiff > 1) {
-        newTailLocation = moveX(headLocation, newTailLocation);
-        newTailLocation = moveY(headLocation, newTailLocation);
-        xDiff = Math.abs(headLocation.x - newTailLocation.x);
-        yDiff = Math.abs(headLocation.y - newTailLocation.y);
-    }
-    return newTailLocation;
+        // get the next knot, I hope this mutates the array
+        let nextKnot = rope[index + 1];
+        let xDiff = Math.abs(knot.x - nextKnot.x);
+        let yDiff = Math.abs(knot.y - nextKnot.y);
+    
+        while (xDiff > 1 || yDiff > 1) {
+            moveX(knot);
+            moveY(knot);
+            xDiff = Math.abs(knot.x - nextKnot.x);
+            yDiff = Math.abs(knot.y - nextKnot.y);
+        }
+        return knot;
+    });
+
 }
 
-function moveX(headLocation, tailLocation) {
-    const newTailLocation = {...tailLocation};
-    let xDiff = Math.abs(headLocation.x - tailLocation.x);
-    let yDiff = Math.abs(headLocation.y - tailLocation.y);
+function moveX(knot) {
+    let xDiff = Math.abs(knot.x - knot.next.x);
+    let yDiff = Math.abs(knot.y - knot.next.y);
     while (xDiff > 1) {
-        if (headLocation.x > tailLocation.x) {
-            newTailLocation.x += 1;
+        if (knot.x > knot.next.x) {
+            knot.next.x += 1;
         } else {
-            newTailLocation.x -= 1;
+            knot.next.x -= 1;
         }
         if (yDiff >= 1) {
-            if (headLocation.y > tailLocation.y) {
-                newTailLocation.y += 1;
+            if (knot.y > knot.next.y) {
+                knot.next.y += 1;
             } else {
-                newTailLocation.y -= 1;
+                knot.next.y -= 1;
             }
         }
-        xDiff = Math.abs(headLocation.x - newTailLocation.x);
+        xDiff = Math.abs(knot.x - knot.next.x);
     }
-    return newTailLocation;
 }
 
-function moveY(headLocation, tailLocation) {
-    const newTailLocation = {...tailLocation};
-    let yDiff = Math.abs(headLocation.y - tailLocation.y);
-    let xDiff = Math.abs(headLocation.x - tailLocation.x);
+function moveY(knot) {
+    let yDiff = Math.abs(knot.y - knot.next.y);
+    let xDiff = Math.abs(knot.x - knot.next.x);
     while (yDiff > 1) {
-        if (headLocation.y > tailLocation.y) {
-            newTailLocation.y += 1;
+        if (knot.y > knot.next.y) {
+            knot.next.y += 1;
         } else {
-            newTailLocation.y -= 1;
+            knot.next.y -= 1;
         }
         if (xDiff >= 1) {
-            if (headLocation.x > tailLocation.x) {
-                newTailLocation.x += 1;
+            if (knot.x > knot.next.x) {
+                knot.next.x += 1;
             } else {
-                newTailLocation.x -= 1;
+                knot.next.x -= 1;
             }
         }
-        yDiff = Math.abs(headLocation.y - newTailLocation.y);
+        yDiff = Math.abs(knot.y - knot.next.y);
     }
-    return newTailLocation;
 }
 
 // part 1 = 6087
